752216be41ca4ecc6d162d5fdbc1a6aa2fa0fc8c
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-11-02 15:21:12 +0530

    001_evenoddnumber

--- solutions/cpp/001_evenOdd.cpp
+++ solutions/cpp/001_evenOdd.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
+using namespace std;
 
 int main() {
-    std::cout << "Hello world, Welcome to CodeBeforeDSA Archive!" << std::endl;
+
+    int number;
+    cout << "Enter the Number: ";
+    cin >> number;
+
+    if (number % 2 == 0) {
+        cout << number << " is EVEN!!";
+    } else {
+        cout << number << " is ODD!";
+    }
+    
     return 0;
 }
