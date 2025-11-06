2ee829275452464f98b194dc3f02a5150b1095f2
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-11-06 21:20:57 +0530

    armstrong done

---
+++ solutions/cpp/004_armstrong.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    long long number;
+    cout << "Enter the Number: ";
+    cin >> number;
+
+    long long temp = number; // copy!!
+
+    // calculating the number of digits first!
+    int numberOfDigits = 0;
+    while (temp > 0) {
+        numberOfDigits++;
+        temp /= 10;
+    }
+
+    temp = number;
+    long long sum = 0;
+    while (temp > 0) {
+        int digit = temp % 10;
+        sum = sum + (pow(digit, numberOfDigits));
+        temp /= 10;
+    }
+
+    if (sum == number) {
+        cout << number << " is AN ARMSTRONG NUMBER!!";
+    } else {
+        cout << number << " is NOT AN ARMSTRONG NUMBER!!!!";
+    }
+
+    return 0;
+}
