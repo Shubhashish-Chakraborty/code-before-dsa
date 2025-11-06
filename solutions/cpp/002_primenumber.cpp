3876b019d85690fee7fb8ff595708cddaac72f1e
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-11-06 12:08:52 +0530

    002 solved

---
+++ solutions/cpp/002_primenumber.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int number;
+    cout << "Enter the Number: ";
+    cin >> number;
+
+    bool isPrime = true;
+
+    if (number <= 1) {
+        isPrime = false;
+    } else {
+        for (int x = 2; x <= number - 1; x++) {
+            if (number % x == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    if (isPrime) {
+        cout << number << " is A PRIME NUMBER!";
+    } else {
+        cout << number << " is NOT A PRIME NUMBER!!";
+    }
+
+
+    return 0;
+}
